f651b596e26e702e1597e71576bffdc304fb10bf
github username <github email address>
2019-07-01 14:19:30 -0400

    seperate the movie links with div.

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,8 +2,13 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const MoviesList = ({ movies }) => {
+
   const renderMovies = Object.keys(movies).map(movieID =>
-    <Link key={movieID} to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
+    <div>
+      
+      <Link key={movieID} to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
+
+    </div>
   );
 
   return (
